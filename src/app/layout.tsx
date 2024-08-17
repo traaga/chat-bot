021c83d5417e6cbb4c586e45956d3e517166be25
traaga <toomas.raaga@gmail.com>
2024-08-17 15:06:10 +0300

    added speedinsights from vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const manrope = Manrope({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={manrope.className}>{children}</body>
+			<SpeedInsights />
 		</html>
 	);
 }
